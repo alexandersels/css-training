049e6f19440d40f1844a3ad8e994e6889f1cb85c
Benjamin.Pinseel <br.pinseel@gmail.com>
2019-07-17 21:27:57 +0200

    feature: Added animations to site.

--- project/shared.js
+++ project/shared.js
@@ -4,15 +4,26 @@ var modalNoButton = document.querySelector(".modal__action--negative");
 var selectPlanButtons = document.querySelectorAll(".plan button");
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
+var ctaButton = document.querySelector(".main-nav__item--cta");
 
+// console.dir(backdrop.style['background-image']);
+
+// console.dir(backdrop);
 for (var i = 0; i < selectPlanButtons.length; i++) {
-    selectPlanButtons[i].addEventListener("click", function () {
+    selectPlanButtons[i].addEventListener("click", function() {
+        // modal.style.display = "block";
+        // backdrop.style.display = "block";
+        // modal.className = 'open'; // This will actually overwrite the complete class list
         modal.classList.add("open");
-        backdrop.classList.add("open");
+        backdrop.style.display = "block";
+        setTimeout(function() {
+            backdrop.classList.add("open");
+        }, 10);
     });
 }
 
-backdrop.addEventListener("click", function () {
+backdrop.addEventListener("click", function() {
+    // mobileNav.style.display = 'none';
     mobileNav.classList.remove("open");
     closeModal();
 });
@@ -22,13 +33,35 @@ if (modalNoButton) {
 }
 
 function closeModal() {
+    //   backdrop.style.display = "none";
+    //   modal.style.display = "none";
     if (modal) {
         modal.classList.remove("open");
     }
     backdrop.classList.remove("open");
+    setTimeout(function() {
+        backdrop.style.display = "none";
+    }, 200);
 }
 
-toggleButton.addEventListener("click", function () {
+toggleButton.addEventListener("click", function() {
+    // mobileNav.style.display = 'block';
+    // backdrop.style.display = 'block';
     mobileNav.classList.add("open");
-    backdrop.classList.add("open");
+    backdrop.style.display = "block";
+    setTimeout(function() {
+        backdrop.classList.add("open");
+    }, 10);
 });
+
+ctaButton.addEventListener('animationstart', function(event) {
+    console.log('Animation started', event);
+})
+
+ctaButton.addEventListener('animationend', function(event) {
+    console.log('Animation ended', event);
+})
+
+ctaButton.addEventListener('animationiteration', function(event) {
+    console.log('Animation iteration', event);
+})
